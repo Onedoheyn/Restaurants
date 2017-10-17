884ed9ef7a10d4bb156745f8f5799d7623637d0a
loloslols <loloslols@nate.com>
2017-10-17 15:00:32 +0900

    CallActivity 추가
    전화번호 버튼시 넘어갈 두번째 액티비티인 CallActivity 추가함

---
+++ app/src/main/java/com/hansung/android/restaurants/CallActivity.java
@@ -0,0 +1,10 @@
+package com.hansung.android.restaurants;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by LeeChanHee on 2017-10-17.
+ */
+
+public class CallActivity extends AppCompatActivity {
+}
