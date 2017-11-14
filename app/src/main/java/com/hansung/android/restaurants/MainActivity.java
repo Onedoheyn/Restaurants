1338c87cbb2e6ce4502b7a148f0783b3053fe2e7
One dohyeon <31878698+Onedoheyn@users.noreply.github.com>
2017-11-14 18:05:47 +0900

    메인,인설트 위치변환
    나오는 순서랑 액티비티 순서를 변환을 주어서 바꿔줌

--- app/src/main/java/com/hansung/android/restaurants/MainActivity.java
+++ app/src/main/java/com/hansung/android/restaurants/MainActivity.java
@@ -1,95 +1,18 @@
 package com.hansung.android.restaurants;
 
-import android.content.ActivityNotFoundException;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.File;
-import java.util.ArrayList;
-
-import android.content.Intent;
-
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-public class MainActivity extends AppCompatActivity{
 
-    private static final int PICK_FROM_CAMERA = 1;
-    private ImageView imgview;
+/**
+ * Created by LeeChanHee on 2017-11-14.
+ */
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.insert);
-        imgview = (ImageView) findViewById(R.id.Camera);
-        ImageButton buttonCamera = (ImageButton) findViewById(R.id.Camera);
-
-
-        buttonCamera.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT,
-                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI.toString());
-
-                try {
-                    intent.putExtra("return-data", true);
-                    startActivityForResult(intent, PICK_FROM_CAMERA);
-                } catch (ActivityNotFoundException e) {
-
-                }
-            }
-        });
-
-        Button btn = (Button) findViewById(R.id.Insert);
-
-        btn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                Intent new_intent = new Intent(getApplicationContext(), InsertActivity.class);
-                startActivity(new_intent);
-            }
-        });
-
-
-    }
-    protected void onActivityResult(int requestCode,
-                                    int resultCode,
-                                    Intent data) {
-
-        if (requestCode == PICK_FROM_CAMERA) {
-            Bundle extras = data.getExtras();
-            if (extras != null) {
-                Bitmap photo = extras.getParcelable("data");
-                imgview.setImageBitmap(photo);
-            }
-        }
-
+        setContentView(R.layout.activity_main);
     }
 }
-//출처: http://mainia.tistory.com/1631 [녹두장군 - 상상을 현실로]
-
 
 
 //
