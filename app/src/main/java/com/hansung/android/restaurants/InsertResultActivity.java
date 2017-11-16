ea3546afc270399053234ccc857caefa29c2ba07
loloslols <loloslols@nate.com>
2017-11-16 17:31:01 +0900

    InsertResultActivity 추가, insetmenu.xml, insertresult.xml 추가
    InsertResultActivity는 맛집 등록을 눌렀을 시 다음 액티비티로 넘어갈 액티비티이고 insertmenu.xml은 음식메뉴를 등록하기 위한 xml이고, insertresult.xml은 맛집 등록을 눌렀을 시 등록된 맛집이 나오게 하는 xml이다

---
+++ app/src/main/java/com/hansung/android/restaurants/InsertResultActivity.java
@@ -0,0 +1,10 @@
+package com.hansung.android.restaurants;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by LeeChanHee on 2017-11-16.
+ */
+
+public class InsertResultActivity extends AppCompatActivity {
+}
